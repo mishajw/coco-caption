b893d0093a4c4054bf51c61371d18c617e475fa3
Misha Wagner <mishajw@gmail.com>
2018-02-05 13:22:50 +0000

    Port code from Python 2 to Python 3

--- pycocoevalcap/__init__.py
+++ pycocoevalcap/__init__.py
@@ -1,1 +1,7 @@
 __author__ = 'tylin'
+from pycocoevalcap.bleu.bleu_scorer import BleuScorer
+from pycocoevalcap.bleu.bleu import Bleu
+from pycocoevalcap.cider.cider import Cider
+from pycocoevalcap.meteor.meteor import Meteor
+from pycocoevalcap.rouge.rouge import Rouge
+from pycocoevalcap.spice.spice import Spice
