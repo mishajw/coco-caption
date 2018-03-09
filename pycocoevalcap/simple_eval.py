b27f51ebc750efa8ab66801dbecacb2b7bfb02ce
Misha Wagner <mishajw@gmail.com>
2018-03-09 14:20:13 +0000

    Add average bleu score metric to simple evaluation function

--- pycocoevalcap/simple_eval.py
+++ pycocoevalcap/simple_eval.py
@@ -1,5 +1,7 @@
 from typing import List, Dict
 
+import numpy as np
+
 from pycocoevalcap import Bleu, Rouge, Cider
 
 __scorers = [
@@ -31,4 +33,17 @@ def simple_eval(predictions: List[str], truths: List[List[str]]) -> Dict[str, Li
                 evaluations[n] = []
             evaluations[n].extend(s)
 
+    __add_average_bleu(evaluations)
     return evaluations
+
+
+def __add_average_bleu(evaluations: Dict[str, List[float]]):
+    bleu_costs: List[List[float]] = []
+    for metric_name in evaluations:
+        if not metric_name.startswith("Bleu"):
+            continue
+        bleu_costs.append(evaluations[metric_name])
+
+    bleu_costs = np.array([evaluations[name] for name in evaluations if name.startswith("Bleu")])
+    average_bleu = np.mean(bleu_costs, axis=0)
+    evaluations["Bleu"] = average_bleu.tolist()
